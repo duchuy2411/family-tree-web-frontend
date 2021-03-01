1926f105444aba04fa99536bfef0c973bff4e47c
Nguyen Dong Hung <ndh1379@gmail.com>
2021-03-01 20:54:11 +0700

    Revert "create boilerplate"
    This reverts commit 20925044b48d523a9a678013938f1dbc517becd7.
    
    revert to add more files for boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from "react";
-import "./App.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import routes from "./pages/routes";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <React.Suspense fallback={<div>Loading . . .</div>}>
-        <Switch>
-          {routes.map(({ component: Component, path, ...rest }, key) => {
-            return (
-              <Route path={path} key={key}>
-                <Component {...rest} />
-              </Route>
-            );
-          })}
-        </Switch>
-      </React.Suspense>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
